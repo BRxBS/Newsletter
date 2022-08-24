d4cd8d82f8926c63fb0494eec878d3a865e6711b
Bruna Santos <brunahta2025@hotmail.com>
2022-08-24 19:06:55 -0300

    subscribe Button

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,15 +1,45 @@
+import { signIn, useSession } from "next-auth/react";
 import React from "react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 import styles from "./styles.module.scss";
 
-export function SubscribeButton(){
-    return(
-        <>
-        <button 
-        type="button"
-        className={styles.subscribeButton}>
+interface SubscribeButton {
+    priceId: string;
+}
 
+export function SubscribeButton({priceId}: SubscribeButton){
+    const { data: session } = useSession();
+
+    async function handleSubscribe(){
+        if (!session) {
+            signIn('github')
+            return
+        }
+
+        try {
+            const response = await api.post('/subscribe')
+
+            const {sessionId} = response.data
+
+            const stripe = await getStripeJs()
+
+            await stripe.redirectToCheckout({ sessionId: sessionId });
+        } catch (error) {
+            alert(error.message )
+        }
+    }
+    
+    return (
+      <>
+        <button
+          type="button"
+          className={styles.subscribeButton}
+          onClick={handleSubscribe}
+        >
+          Subscribe Now
         </button>
-        </>
-    )
+      </>
+    );
 }
---
+++ src/services/stripe-js.ts
@@ -0,0 +1,9 @@
+// stripe no front-end
+
+import { loadStripe} from '@stripe/stripe-js'
+
+export async function getStripeJs(){
+    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    return stripeJs
+}
