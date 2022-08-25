96eb9beff028345aa12f05ff26be55f3e0788599
Bruna Santos <brunahta2025@hotmail.com>
2022-08-25 19:26:26 -0300

    stripe front and back

--- src/services/stripe-js.ts
+++ src/services/stripe-js.ts
@@ -1,9 +1,15 @@
 // stripe no front-end
-
 import { loadStripe} from '@stripe/stripe-js'
 
-export async function getStripeJs(){
-    const stripeJs = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
 
-    return stripeJs
+let stripePromisse = null
+
+export async function getStripeJs(){
+
+    if (!stripePromisse) {
+        stripePromisse = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+    }
+
+
+    return stripePromisse
 }
--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -1,12 +1,10 @@
 import Stripe from 'stripe'
-import {version} from '../../package.json'
+//import {version} from '../../package.json'
 
-export const stripe = new Stripe(process.env.STRIPE_KEY,
-    {
-        apiVersion:'2022-08-01',
-        appInfo:{
-            name: 'Bruna dos Santos',
-            version
-        }
-
-    });
+export const stripe = new Stripe(process.env.STRIPE_KEY as string, {
+  apiVersion: "2022-08-01",
+  appInfo: {
+    name: "Bruna dos Santos",
+    version: "1.3.0",
+  },
+});
