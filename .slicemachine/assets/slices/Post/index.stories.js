86887821eff53a9e9ae31630efda8a562c60b139
Bruna Santos <brunahta2025@hotmail.com>
2022-08-29 18:54:04 -0300

    prismic

---
+++ .slicemachine/assets/slices/Post/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/Post';
+
+export default {
+  title: 'slices/Post'
+}
+
+
+export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"heading1","text":"Shelter","spans":[]}],"description":[{"type":"paragraph","text":"Aute commodo sunt aliqua laboris ut ipsum duis et. Non excepteur esse qui culpa qui magna fugiat aliqua laborum ullamco ipsum ad. Id consectetur nulla aliquip dolor culpa ullamco tempor irure minim in.","spans":[]}]},"slice_type":"post","id":"_Default"}} />
+_Default.storyName = ''
