a214cd8d1c98289277e77a013b614d486ab2f1dc
Bruna Santos <brunahta2025@hotmail.com>
2022-10-13 19:58:07 -0300

    sessionWithFaunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,10 +4,21 @@ import { env } from "process";
 import { getToken } from "next-auth/jwt";
 import { query } from "faunadb";
 import { fauna } from "../../../services/fauna";
+import { v4 as uuid } from "uuid";
+import { Client as FaunaClient } from "faunadb"
+import { FaunaAdapter } from "@next-auth/fauna-adapter"
+
+// const client = new FaunaClient({
+//   secret: process.env.FAUNA_KEY,
+//   scheme: "http",
+//   domain: "localhost",
+//   port: 3000,
+// });
 
 export default NextAuth({
   // Configure one or more authentication providers
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
+  // adapter: FaunaAdapter(client),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID,
@@ -21,12 +32,41 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+
   callbacks: {
+    // async jwt({ token, account }) {
+    //   // Persist the OAuth access_token to the token right after signin
+    //   if (account) {
+    //     token.accessToken = account.access_token
+    //   }
+    //   return token
+    // },
+     async session( {session }) {
+      session.user.email
+      const userActiveSubscription = await fauna.query(
+        query.Get(
+            query.Match(
+              query.Index('subscription_by_user_ref'),
+              query.Select(
+                'ref',
+                query.Get(
+                  query.Match(
+                    query.Index('user_by_email'),
+                    query.Casefold(session.user.email)
+                  )
+                )
+              )
+            )
+        )
+      )
+       return session;
+     },
+
     async signIn({ user, account, profile, credentials }) {
-        const {email} = user
+      const { email } = user;
       try {
         await fauna.query(
-         query.If(
+          query.If(
             query.Not(
               query.Exists(
                 query.Match(
@@ -35,10 +75,8 @@ export default NextAuth({
                 )
               )
             ),
-            query.Create(query.Collection("users"),
-             { data: { email } }
-            ),
-           query.Get(
+            query.Create(query.Collection("users"), { data: { email } }),
+            query.Get(
               query.Match(
                 query.Index("user_by_email"),
                 query.Casefold(user.email)
@@ -46,8 +84,7 @@ export default NextAuth({
             )
           )
         );
-        
-        
+
         return true;
       } catch {
         return false;
