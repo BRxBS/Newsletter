86b09456e703a4fc9a45b8d382ae1b9e3808cb3c
Bruna Santos <brunahta2025@hotmail.com>
2022-10-13 19:50:31 -0300

    cleaning

--- .slicemachine/assets/slices/Post/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Post';
-
-export default {
-  title: 'slices/Post'
-}
-
-
-export const _Default = () => <MyComponent slice={{"variation":"default","version":"sktwi1xtmkfgx8626","items":[{}],"primary":{"title":[{"type":"paragraph","text":"Ex est qui aute ullamco proident. Et adipisicing fugiat est elit ad sit non dolore cupidatat mollit. In sint eu adipisicing sunt cillum esse cillum officia.","spans":[]}],"component":[{"type":"paragraph","text":"Consequat adipisicing consequat occaecat esse. Quis sunt non adipisicing irure exercitation excepteur eiusmod duis proident id dolor eu quis consectetur nostrud.","spans":[]}]},"slice_type":"post","id":"_Default"}} />
-_Default.storyName = ''
