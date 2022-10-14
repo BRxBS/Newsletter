27a6b65c41343395cc126d353dda5e5f55767e0e
Bruna Santos <brunahta2025@hotmail.com>
2022-10-14 19:14:43 -0300

    google-provider

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
 import { env } from "process";
 import { getToken } from "next-auth/jwt";
 import { query } from "faunadb";
@@ -8,63 +8,37 @@ import { v4 as uuid } from "uuid";
 import { Client as FaunaClient } from "faunadb"
 import { FaunaAdapter } from "@next-auth/fauna-adapter"
 
-// const client = new FaunaClient({
-//   secret: process.env.FAUNA_KEY,
-//   scheme: "http",
-//   domain: "localhost",
-//   port: 3000,
-// });
+ const client = new FaunaClient({
+   secret: process.env.FAUNA_KEY,
+   scheme: "http",
+   domain: "localhost",
+   port: 3000,
+   });
 
 export default NextAuth({
   // Configure one or more authentication providers
   secret: process.env.NEXTAUTH_SECRET,
   // adapter: FaunaAdapter(client),
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      authorization: {
-        params: {
-          // I wish to request additional permission scopes.
-          scope: "read-user",
-        },
-      },
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
     // ...add more providers here
   ],
 
+  adapter: FaunaAdapter(client),
   callbacks: {
-    // async jwt({ token, account }) {
-    //   // Persist the OAuth access_token to the token right after signin
-    //   if (account) {
-    //     token.accessToken = account.access_token
-    //   }
-    //   return token
-    // },
-     async session( {session }) {
-      session.user.email
-      const userActiveSubscription = await fauna.query(
-        query.Get(
-            query.Match(
-              query.Index('subscription_by_user_ref'),
-              query.Select(
-                'ref',
-                query.Get(
-                  query.Match(
-                    query.Index('user_by_email'),
-                    query.Casefold(session.user.email)
-                  )
-                )
-              )
-            )
-        )
-      )
-       return session;
-     },
+
+
+
 
     async signIn({ user, account, profile, credentials }) {
       const { email } = user;
       try {
+        if (account.provider === "google") {
+          return profile.email_verified && profile.email.endsWith("@gmail.com")
+        }
         await fauna.query(
           query.If(
             query.Not(
