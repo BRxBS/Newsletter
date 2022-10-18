59114ba793ceb188fa124398fba933d39dea029f
Bruna Santos <brunahta2025@hotmail.com>
2022-10-18 18:30:47 -0300

    FaunaAdapter(client)-getting in the way

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,15 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import { env } from "process";
-import { getToken } from "next-auth/jwt";
 import { query } from "faunadb";
 import { fauna } from "../../../services/fauna";
-import { v4 as uuid } from "uuid";
 import { Client as FaunaClient } from "faunadb"
 import { FaunaAdapter } from "@next-auth/fauna-adapter"
 
  const client = new FaunaClient({
-   secret: process.env.FAUNA_KEY,
-   scheme: "http",
-   domain: "localhost",
-   port: 3000,
+  secret:  process.env.FAUNA_KEY,
+  scheme: "http",
+  domain: "localhost",
+  port: 3000,
    });
 
 export default NextAuth({
@@ -27,38 +24,36 @@ export default NextAuth({
     // ...add more providers here
   ],
 
-  adapter: FaunaAdapter(client),
+  // adapter: FaunaAdapter(client),
+
   callbacks: {
-
-
 
 
     async signIn({ user, account, profile, credentials }) {
       const { email } = user;
       try {
-        if (account.provider === "google") {
-          return profile.email_verified && profile.email.endsWith("@gmail.com")
-        }
         await fauna.query(
           query.If(
-            query.Not(
-              query.Exists(
-                query.Match(
-                  query.Index("user_by_email"),
-                  query.Casefold(user.email)
-                )
-              )
-            ),
-            query.Create(query.Collection("users"), { data: { email } }),
+             query.Not(
+               query.Exists(
+                 query.Match(
+                   query.Index("user_by_email"),
+                   query.Casefold(user.email)
+                 )
+               )
+             ),
+             query.Create(query.Collection("users"),
+              { data: { email } }
+             ),
             query.Get(
-              query.Match(
-                query.Index("user_by_email"),
-                query.Casefold(user.email)
-              )
-            )
-          )
-        );
-
+               query.Match(
+                 query.Index("user_by_email"),
+                 query.Casefold(user.email)
+               )
+             )
+           )
+         );
+          
         return true;
       } catch {
         return false;
