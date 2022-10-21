03834c11d1af6edaceee38c41835706e9f32eff3
Bruna Santos <brunahta2025@hotmail.com>
2022-10-21 14:23:56 -0300

    manegeSub-not-conecting-with-fauna

--- src/pages/api/_lib/manegeSubscription.ts
+++ src/pages/api/_lib/manegeSubscription.ts
@@ -1,18 +1,41 @@
 import { fauna } from "../../../services/fauna";
 import { query } from "faunadb";
 import { stripe } from "../../../services/stripe";
+import { getSession } from "next-auth/react";
+import { NextApiRequest, NextApiResponse } from "next";
+
+type User = {
+    ref: {
+      id: string;
+    };
+    data: {
+        stripe_customer_id: string;
+    };
+}
 
 
 export async function  saveSubscription(
     subscriptionId: string,
     customerId: String,
-    createAction = false,
-){
-  console.log('subscriptionId', subscriptionId)
-  console.log('customerId', customerId)
-    const userRef =  await fauna.query(
+    createAction = true,
+ ){
+//     console.log(vari)
+//   console.log('subscriptionId', subscriptionId)
+//  console.log('customerId', customerId)
+                                                                                                          
+console.log('depois session')
+
+const user = await fauna.query<User>(
+        query.Get(
+            query.Match(
+                query.Index('user_by_stripe_customer_id'),
+                query.Casefold(customerId)
+            )
+        )
+    )
+    const userRef =  await fauna.query<User>(
         query.Select(
-            'ref',
+            query.Ref(query.Collection("users"), user.ref.id),
             query.Get(
                 query.Match(
                     query.Index('user_by_stripe_customer_id'),
@@ -21,22 +44,84 @@ export async function  saveSubscription(
             )
         )
     )
+    console.log('customerId- sub', customerId)
+    console.log('user.ref.id', user.ref.id)
 
     const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+    console.log('const subscription ', subscription)
 
+  
     const subscriptionData = {
         id: subscription.id,
         userId: userRef,
         status: subscription.status,
         price_id: subscription.items.data[0].price.id
     }
-
+    if (createAction) {
     await fauna.query(
-      query.Create(
-        query.Collection('subscriptions'),
-        {data: subscriptionData}
-      )
-    )
+      query.Create(query.Collection("subscriptions"), {
+        data: subscriptionData,
+      })
+    );
+} else{
+        await fauna.query(
+          query.Replace(
+            query.Select(
+                user.ref.id,
+              query.Get(
+                query.Match(
+                    query.Index("subscription_by_id"),
+                    subscriptionId,
+                 )
+              )
+            ),
+            {data: subscriptionData}
+          )
+        );
+}
+    
+    await fauna.query(
+        query.Update(query.Ref(query.Collection("subscriptions"), user.ref.id), {
+          data: {subscriptionData},
+        })
+      );
+//  
+
+// console.log('user ', user)
+
+//     const userRef =  await fauna.query<User>(
+//         query.Select(
+//             query.Ref(query.Collection("users"), user.ref.id),
+//             query.Get(
+//                 query.Match(
+//                     query.Index('user_by_stripe_customer_id'),
+//                     customerId
+//                 )
+//             )
+//         )
+//     )
+//     console.log('userRef ', userRef)
+
+//     const subscription = await stripe.subscriptions.retrieve(subscriptionId)
+//     console.log('const subscription ', subscription)
+
+//     const subscriptionData = {
+//         id: subscription.id,
+//         userId: userRef,
+//         status: subscription.status,
+//         price_id: subscription.items.data[0].price.id
+//     }
+//     console.log('subscriptionData', subscriptionData)
+
+//     await fauna.query(
+//       query.Create(
+//         query.Collection('subscriptions'),
+//         {data: subscriptionData}
+//       )
+//     )
+
+
+
 
 //     console.log("subscriptionData", subscriptionData);
 
@@ -63,3 +148,4 @@ export async function  saveSubscription(
 //         );
 // }
 }
+    
